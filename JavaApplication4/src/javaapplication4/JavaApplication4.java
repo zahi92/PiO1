a8e5f1f4a0dd18b3326772a97ca670d479aa1018
Student <Student@10.5.4.13>
2013-10-15 14:07:43 +0200

    nowy

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,16 @@
+
+package javaapplication4;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
